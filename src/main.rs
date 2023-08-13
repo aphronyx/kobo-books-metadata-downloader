48e463f5b3426af6cf235b1096f85434b20807cd
Aphronyx <aphronyx.su@gmail.com>
2023-08-13 12:41:45 +0800

    feat: get book id from url

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,8 @@ struct Book {
 }
 
 fn main() -> Result<()> {
+    let mut book_ids = Vec::<String>::new();
+
     println!("Enter Kobo book URLs:");
     loop {
         let mut input = String::new();
@@ -34,7 +36,27 @@ fn main() -> Result<()> {
             println!("Done!");
             break;
         }
+
+        let Some(book_id) = get_book_id(&input) else {
+            println!("Not a Kobo book URL!");
+            continue;
+        };
+        book_ids.push(book_id);
     }
 
     Ok(())
+}
+
+fn get_book_id(input: &str) -> Option<String> {
+    let is_not_kobo_book_url = !input.contains("https://www.kobo.com/tw/zh/ebook/");
+    if is_not_kobo_book_url {
+        return None;
+    }
+
+    let book_id = input
+        .rsplit_once('/')
+        .map(|(_, substring)| substring.trim().to_string())
+        .filter(|id| !id.is_empty())?;
+
+    Some(book_id)
 }
