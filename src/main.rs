6650a04ce9da1da0ad53ca6a18573d5a1919a7f9
Aphronyx <aphronyx.su@gmail.com>
2023-08-13 12:26:46 +0800

    feat: define enum `Rating` and struct `Book`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+enum Rating {
+    NotRated,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+}
+
+struct Book {
+    id: String,
+    title: String,
+    authors: String,
+    series_name: Option<String>,
+    series_index: Option<f64>,
+    cover: String,
+    synopsis: String,
+    tags: String,
+    rating: Rating,
+    publisher: String,
+    release_date: String,
+    language: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
